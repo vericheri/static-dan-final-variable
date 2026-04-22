d90d1776b2bcef46cf68647913b92f9283a9c9dc
Aurella Meidista Septiasa <aurellinda703@gmail.com>
2026-04-22 11:52:38 +0700

    latihan 2 dan 3

---
+++ final_class.php
@@ -0,0 +1,8 @@
+<?php
+final class database {
+    public function connect() {
+        echo "Koneksi Database";
+    }
+}
+
+?>
---
+++ static_method.php
@@ -0,0 +1,9 @@
+<?php
+class matematika {
+    public static function tambah ($a, $b) {
+        return $a + $b;
+    }
+}
+
+echo matematika::tambah(5, 3);
+?>
